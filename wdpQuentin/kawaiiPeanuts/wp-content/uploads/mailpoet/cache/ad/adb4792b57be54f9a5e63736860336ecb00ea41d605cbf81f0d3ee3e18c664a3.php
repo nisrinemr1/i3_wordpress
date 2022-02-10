853b81ece9d40ff8bce1c350754e0c623282d810
nisrinemr1 <nisrinemr1@gmail.com>
2022-02-10 10:35:27 +0100

    wordpress exercices

---
+++ wdpQuentin/kawaiiPeanuts/wp-content/uploads/mailpoet/cache/ad/adb4792b57be54f9a5e63736860336ecb00ea41d605cbf81f0d3ee3e18c664a3.php
@@ -0,0 +1,55 @@
+<?php
+
+use MailPoetVendor\Twig\Environment;
+use MailPoetVendor\Twig\Error\LoaderError;
+use MailPoetVendor\Twig\Error\RuntimeError;
+use MailPoetVendor\Twig\Extension\SandboxExtension;
+use MailPoetVendor\Twig\Markup;
+use MailPoetVendor\Twig\Sandbox\SecurityError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedTagError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFilterError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use MailPoetVendor\Twig\Source;
+use MailPoetVendor\Twig\Template;
+
+/* newsletter/templates/blocks/unknownBlockFallback/widget.hbs */
+class __TwigTemplate_328093cf144828b5a58988af5cf2f65b4e1633bfa859368f39fc746481d450d8 extends \MailPoetVendor\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"mailpoet_widget_title\"></div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "newsletter/templates/blocks/unknownBlockFallback/widget.hbs";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "newsletter/templates/blocks/unknownBlockFallback/widget.hbs", "/Applications/MAMP/htdocs/kawaiiPeanuts/wp-content/plugins/mailpoet/views/newsletter/templates/blocks/unknownBlockFallback/widget.hbs");
+    }
+}
