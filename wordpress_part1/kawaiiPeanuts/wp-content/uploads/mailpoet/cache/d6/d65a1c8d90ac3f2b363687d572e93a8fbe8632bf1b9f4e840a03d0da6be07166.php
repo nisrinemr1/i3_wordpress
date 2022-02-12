2850cb76760b8167f4b8aaf66e9cce044b923eb3
nisrinemr1 <nisrinemr1@gmail.com>
2022-02-12 18:24:59 +0100

    name file has been changed

---
+++ wordpress_part1/kawaiiPeanuts/wp-content/uploads/mailpoet/cache/d6/d65a1c8d90ac3f2b363687d572e93a8fbe8632bf1b9f4e840a03d0da6be07166.php
@@ -0,0 +1,57 @@
+<?php
+
+use MailPoetVendor\Twig\Environment;
+use MailPoetVendor\Twig\Error\LoaderError;
+use MailPoetVendor\Twig\Error\RuntimeError;
+use MailPoetVendor\Twig\Extension\SandboxExtension;
+use MailPoetVendor\Twig\Markup;
+use MailPoetVendor\Twig\Sandbox\SecurityError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedTagError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFilterError;
+use MailPoetVendor\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use MailPoetVendor\Twig\Source;
+use MailPoetVendor\Twig\Template;
+
+/* newsletter/templates/blocks/products/block.hbs */
+class __TwigTemplate_383f62d04358f09834aae5c768ff7b6ba212f15b33164c1d06624102d89b2132 extends \MailPoetVendor\Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div class=\"mailpoet_tools\"></div>
+<div class=\"mailpoet_products_container\"></div>
+<div class=\"mailpoet_block_highlight\"></div>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "newsletter/templates/blocks/products/block.hbs";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "newsletter/templates/blocks/products/block.hbs", "/Applications/MAMP/htdocs/kawaiiPeanuts/wp-content/plugins/mailpoet/views/newsletter/templates/blocks/products/block.hbs");
+    }
+}
